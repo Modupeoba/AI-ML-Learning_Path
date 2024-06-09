42a4aff88d8cc69e4671340674fb204757c028e8
Modupeoba <162997237+Modupeoba@users.noreply.github.com>
2024-06-09 20:52:47 +0100

    Update house.py

--- house.py
+++ house.py
@@ -8,9 +8,6 @@ import warnings
 warnings.filterwarnings('ignore')
 import joblib 
 
-import streamlit as st
-import pandas as pd
-
 # Add a title with icon
 st.title(":house_with_garden: USA Housing Insights: Unveiling the Secrets of Real Estate Trends")
 st.markdown("<br>", unsafe_allow_html=True)
@@ -18,12 +15,13 @@ st.markdown("<br>", unsafe_allow_html=True)
 # Add an image
 st.image('modern-residential-district-with-green-roof-balcony-generated-by-ai-removebg-preview.png')
 
+
 # Add image to sidebar
-st.sidebar.image('3999989__1_-removebg-preview.png', width = 300, caption = 'Welcome User')
+st.sidebar.image('3999989__1_-removebg-preview.png', width=300, caption='Welcome User')
 
 # Add divider and spacing
 st.sidebar.divider()
-st.sidebar.markdown("<br>", unsafe_allow_html= True)
+st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
 # Add a header for project background information with styled divider
 st.write(
@@ -39,66 +37,98 @@ st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 
 # Load the housing dataset
-data = pd.read_csv('USA_Housing.csv')
+df = pd.read_csv('USA_Housing.csv')
 
 # Drop the 'Address' column from the dataset
-data = data.drop('Address', axis=1)
+df = df.drop('Address', axis=1)
 
-# Display the modified housing dataset in the Streamlit app
-st.dataframe(data)
-
-# Declare user Input variables with styled divider
-st.sidebar.write(
-    f"<h3 style='border-bottom:2px solid green; padding-bottom:10px;'>Input Variables</h3>", 
+# Dataset Overview
+st.write(
+    f"<h2 style='border-bottom:2px solid green; padding-bottom:10px;'>Dataset Overview</h2>", 
     unsafe_allow_html=True
 )
-avg_area_income = st.sidebar.number_input('Avg. Area Income', min_value=data['Avg. Area Income'].min(), max_value=data['Avg. Area Income'].max())
-avg_area_house_age = st.sidebar.number_input('Avg. Area House Age', min_value=data['Avg. Area House Age'].min(), max_value=data['Avg. Area House Age'].max())
-avg_area_rooms = st.sidebar.number_input('Avg. Area Number of Rooms', min_value=data['Avg. Area Number of Rooms'].min(), max_value=data['Avg. Area Number of Rooms'].max())
-avg_area_bedrooms = st.sidebar.number_input('Avg. Area Number of Bedrooms', min_value=data['Avg. Area Number of Bedrooms'].min(), max_value=data['Avg. Area Number of Bedrooms'].max())
-area_population = st.sidebar.number_input('Area Population', min_value=data['Area Population'].min(), max_value=data['Area Population'].max())
 
-# Display the user's input
-input_var = pd.DataFrame()
-input_var['Avg. Area Income'] = [avg_area_income]
-input_var['Avg. Area House Age'] = [avg_area_house_age]
-input_var['Avg. Area Number of Rooms'] = [avg_area_rooms]
-input_var['Avg. Area Number of Bedrooms'] = [avg_area_bedrooms]
-input_var['Area Population'] = [area_population]
+if st.checkbox('Show Raw Data'):
+    st.write(df)
 
-st.markdown("<br>", unsafe_allow_html= True)
 
-# display the users input variable 
-st.subheader('Users Input Variables')
-st.dataframe(input_var)
+st.sidebar.header('User Input Parameters')
 
-# Add a button to trigger prediction
-predict_button = st.sidebar.button('Predict')
+# Function to get user input
+def get_user_input():
+    input_choice = st.sidebar.radio('Choose Your Input Type', ['Slider Input', 'Number Input'])
+    
+    if input_choice == 'Slider Input':
+        area_income = st.sidebar.slider('Average Area Income', float(df['Avg. Area Income'].min()), float(df['Avg. Area Income'].max()))
+        house_age = st.sidebar.slider('Average House Age', float(df['Avg. Area House Age'].min()), float(df['Avg. Area House Age'].max()))
+        room_num = st.sidebar.slider('Average Number of Rooms', float(df['Avg. Area Number of Rooms'].min()), float(df['Avg. Area Number of Rooms'].max()))
+        bedrooms = st.sidebar.slider('Average Number of Bedrooms', float(df['Avg. Area Number of Bedrooms'].min()), float(df['Avg. Area Number of Bedrooms'].max()))
+        population = st.sidebar.slider('Area Population', float(df['Area Population'].min()), float(df['Area Population'].max()))
+    else:
+        area_income = st.sidebar.number_input('Average Area Income', float(df['Avg. Area Income'].min()), float(df['Avg. Area Income'].max()))
+        house_age = st.sidebar.number_input('Average House Age', float(df['Avg. Area House Age'].min()), float(df['Avg. Area House Age'].max()))
+        room_num = st.sidebar.number_input('Average Number of Rooms', float(df['Avg. Area Number of Rooms'].min()), float(df['Avg. Area Number of Rooms'].max()))
+        bedrooms = st.sidebar.number_input('Average Number of Bedrooms', float(df['Avg. Area Number of Bedrooms'].min()), float(df['Avg. Area Number of Bedrooms'].max()))
+        population = st.sidebar.number_input('Area Population', float(df['Area Population'].min()), float(df['Area Population'].max()))
+    
+    user_df = {
+        'Avg. Area Income': area_income,
+        'Avg. Area House Age': house_age,
+        'Avg. Area Number of Rooms': room_num,
+        'Avg. Area Number of Bedrooms': bedrooms,
+        'Area Population': population
+    }
+    
+    features = pd.DataFrame(user_df, index=[0])
+    return features
 
-# Load the scaler models
-avg_area_income_scaler = joblib.load('Avg. Area Income_scaler.pkl')
-area_population_scaler = joblib.load('Area Population_scaler.pkl')
+# Get user input
+user_input = get_user_input()
 
-# Transform the users input with the imported scalers
-input_var['Avg. Area Income'] = avg_area_income_scaler.transform(input_var[['Avg. Area Income']])
-input_var['Area Population'] = area_population_scaler.transform(input_var[['Area Population']])
-
-# # Display the scaled input variables along with their original values
-# st.subheader('Scaled Input Variables')
-# st.write(input_var)
-
-# Display the predicted value
+# Load the scalers and model
+area_pop_scaler = joblib.load('Area Population_scaler.pkl')
+area_inc_scaler = joblib.load('Avg. Area Income_scaler.pkl')
 model = joblib.load('HousepriceModel.pkl')
-predicted = model.predict(input_var)
 
-# Add spacing using Markdown
-st.markdown("<br>", unsafe_allow_html= True)
-st.markdown("<br>", unsafe_allow_html= True)
+# Transform user input
+user_input['Area Population'] = area_pop_scaler.transform(user_input[['Area Population']])
+user_input['Avg. Area Income'] = area_inc_scaler.transform(user_input[['Avg. Area Income']])
 
-# Add a subheader for the predicted value
-st.markdown("<h2 style='color: #00541A;'>Predicted House Price</h2>", unsafe_allow_html=True)
-
-# Display the predicted value
-st.write(f"The predicted house price is: ${predicted[0]:,.2f}")
+# Predict house price
+predicted = model.predict(user_input)
 
 
+# Tabs for prediction and interpretation
+prediction, interpretation = st.tabs(["Prediction", "Interpretation"])
+
+with prediction:
+    pred = st.button('Predict')
+    if pred:
+        st.success(f'The Predicted price of your house is ${predicted[0]:,.2f} dollars')
+
+with interpretation:
+    st.header('Interpretation')
+    st.write(f'The intercept of the model is: ${round(model.intercept_, 2):,.2f}')
+    st.write(f'A unit change in the average area income causes the price to change by ${round(model.coef_[0], 2):,.2f}')
+    st.write(f'A unit change in the average house age causes the price to change by ${round(model.coef_[1], 2):,.2f}')
+    st.write(f'A unit change in the average rooms causes the price to change by ${round(model.coef_[2], 2):,.2f}')
+    st.write(f'A unit change in the average bedrooms causes the price to change by ${round(model.coef_[3], 2):,.2f}')
+    st.write(f'A unit change in the area population causes the price to change by ${round(model.coef_[4], 2):,.2f}')
+
+
+# User Guide and Help Section
+st.header('User Guide & Help')
+
+if st.checkbox('Show User Guide'):
+    st.subheader('User Guide')
+    st.write("""
+    - Use the sliders or number inputs to provide average area income, house age, number of rooms, bedrooms, and population.
+    - Click 'Predict' to see the predicted house price.
+    - The 'Interpretation' section explains how each feature affects the predicted price.
+    - Check 'Show Raw Data'to explore the dataset.
+    """)
+
+    st.subheader('Need Help?')
+    st.write("""
+    - If you encounter any issues or have questions, please contact our support team at modupeobamuyi@gmail.com
+    """)
